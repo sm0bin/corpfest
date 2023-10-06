46930864c4815454d4e552093b332525b0c14ae1
GhostMobin <s1810677110@ru.ac.bd>
2023-10-06 23:38:31 +0600

    first commit

---
+++ src/pages/shared/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <aside>
+                <p>Copyright © CorpFest 2023 - All right reserved by CorpFest Ltd</p>
+            </aside>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layouts/Root";
+import LoginPage from "../pages/auth/LoginPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        children: [
+            {
+                path: "/login",
+                element: <LoginPage></LoginPage>,
+            }
+        ]
+    },
+]);
+
+export default router;
